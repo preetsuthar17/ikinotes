7cc0a444960464f9e8078ce5cc3dce9c3147b0a4
preetsuthar17 <preetsutharxd@gmail.com>
2025-07-15 22:56:59 +0530

    Integrate Cascadia Code font into RootLayout component and add external script for enhanced analytics tracking.

--- src/app/new/layout.tsx
+++ src/app/new/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
+import { Cascadia_Code } from "next/font/google";
 import "../globals.css";
 import { Suspense } from "react";
+
+const cascadiaMono = Cascadia_Code({
+  variable: "--font-cascadia-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "beautiful open-source note taking application — Iki",
@@ -14,7 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>
+      <head>
+        <script
+          defer
+          src="https://assets.onedollarstats.com/stonks.js"
+        ></script>
+      </head>
+      <body className={` ${cascadiaMono.className} antialiased`}>
         <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
       </body>
     </html>
